62e5adbee1991905cb5c1cb186df12713364c1dd
Nhat Nguyen <phongnhat19@gmail.com>
2020-04-21 18:13:04 +0700

    persist user data in hook

--- modules/task-management/dist/hooks/before_incoming_middleware/setDomain.js
+++ modules/task-management/dist/hooks/before_incoming_middleware/setDomain.js
@@ -1,10 +1,16 @@
 const setDomain = async () => {
   if (event.type === 'set-domain') {
     const eventData = event.payload
-    const domain = eventData.payload.payload.domain
-    try {
+    const domain = eventData.payload.domain || eventData.payload.payload.domain
 
+    // Skip event processing
+    event.setFlag(bp.IO.WellKnownFlags.SKIP_DIALOG_ENGINE, true)
+    // Force persist state
+    event.setFlag(bp.IO.WellKnownFlags.FORCE_PERSIST_STATE, true)
+
+    try {
       event.state.user['domain'] = domain
+      bp.logger.info(`Domain of user ${event.target} set to ${event.state.user['domain']}`);
 
       const payloads = await bp.cms.renderElement('#!builtin_text-IRmEbB', {}, event)
       await bp.events.replyToEvent(event, payloads)
