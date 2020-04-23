4519f16d5a62f29a4b7a648adf8e24fe514c7815
Nhat Nguyen <phongnhat19@gmail.com>
2020-04-23 14:04:06 +0700

    update kintone service module

--- modules/task-management/dist/actions/kintone.js
+++ modules/task-management/dist/actions/kintone.js
@@ -4,15 +4,36 @@ const CONFIG = {
   TASK_MANAGEMENT: {
     ASSIGNEE_FIELD_CODE: 'User_selection_3',
     STATUS_FIELD_CODE: 'Drop_down_1',
-    DUE_DATE_FIELD_CODE: 'Date'
+    DUE_DATE_FIELD_CODE: 'Date',
+    SUMMARY_FIELD_CODE: 'Text',
+    RECORD_NUMBER_FIELD_CODE: 'Record_number'
   }
 }
-
+/**
+ * API to get task from kintone
+ * @title API to get task from kintone
+ * @category Task Management
+ * @author Nhat Nguyen
+ * @param {string} assignee - The assignee (user code) of task
+ * @param {string} status - The status of task
+ * @param {Date} dueDate - The due date of task
+ * @param {any} user - The due date of task
+ */
 const getTask = async (assignee, status, dueDate, user) => {
   const appID = user['taskAppID']
   const apiToken = user['taskAppAPIToken'];
 
   const URL = `${CONFIG.DOMAIN}/k/v1/records.json`
+  let query = `${CONFIG.TASK_MANAGEMENT.ASSIGNEE_FIELD_CODE} in ("${assignee}")`
+
+  if (status && status !== 'All') {
+    query += ` and ${CONFIG.TASK_MANAGEMENT.STATUS_FIELD_CODE} in ("${status}")`;
+  }
+
+  if (dueDate) {
+    const dateString = `${dueDate.getFullYear()}-${dueDate.getMonth() + 1}-${dueDate.getDate()}`
+    query += ` and ${CONFIG.TASK_MANAGEMENT.DUE_DATE_FIELD_CODE} >= "${dateString}"`
+  }
 
   const { data } = await axios({
     method: 'get',
@@ -22,7 +43,7 @@ const getTask = async (assignee, status, dueDate, user) => {
     },
     params: {
       app: appID,
-      query: `${CONFIG.TASK_MANAGEMENT.ASSIGNEE_FIELD_CODE} in ("${assignee}")`
+      query
     }
   })
 
@@ -30,5 +51,9 @@ const getTask = async (assignee, status, dueDate, user) => {
 }
 
 module.exports = {
-  getTask
+  getTask,
+  FIELD_CODE: {
+    TASK_MANAGEMENT: CONFIG.TASK_MANAGEMENT
+  },
+  DOMAIN: CONFIG.DOMAIN
 }
